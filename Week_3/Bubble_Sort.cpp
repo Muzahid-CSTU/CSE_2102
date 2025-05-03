7998ada0c40bb9f10adf649bfdb278de952eabe2
Muzahid <kmujahidulislamm@gmail.com>
2025-05-03 21:39:55 +0600

    Update Bubble_Sort.cpp

--- Week_3/Bubble_Sort.cpp
+++ Week_3/Bubble_Sort.cpp
@@ -1,23 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
+    cout << "Enter array size: " << endl;
     cin >> n;
     int ar[n];
-    for(int i=0;i<n;i++){
+    cout << "Enter array elements " << endl;
+    for (int i = 0;i < n;i++)
+    {
         cin >> ar[i];
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(ar[i] > ar[j]){
-                int t = ar[i];
-                ar[i] = ar[j];
-                ar[j] = t;
+    int i = n - 1;
+    while (i)
+    {
+        int flg = 1;
+        for (int j = 0; j < i;j++)
+        {
+            if (ar[j] > ar[j + 1])
+            {
+                flg = 0;
+                swap(ar[j], ar[j + 1]);
             }
         }
+        if (flg){
+            break;
+        }
+        i--;
     }
-    for(int i=0;i<n;i++){
+    cout << "Sorted in ascending order: ";
+    for (int i = 0; i < n;i++)
+    {
         cout << ar[i] << " ";
     }
+    return 0;
 }
